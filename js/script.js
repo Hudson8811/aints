9c2182ddcc66028ed69b8f718260baac5aa11cd4
Hudson8811 <hudson88@yandex.ru>
2019-05-06 19:49:51 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$('.header .btn-login').click(function(){
+  $('#login_form').arcticmodal();
+ });
+ 
+ $('.modal--form .btn-login').click(function(){
+  $('#login_form').arcticmodal();
+  $('#regisry_form').arcticmodal("close");
+ });
+
+$('.btn-regisrty').click(function(){
+  $('#regisry_form').arcticmodal();
+  $('#login_form').arcticmodal("close");
+ });
+ 
+ $('.modal--form .forget_pass').click(function(){
+  $('#login_form').arcticmodal("close");
+  $('#reset__pass').arcticmodal();
+ });
+ 
+$(document).ready(function() {
+  $('.select').niceSelect();
+})
+
+
+
+$(".numbox").mask("+7 (999) 999-99-99");
+
+
+
+$(function() {
+$('.btn11').click(function() {
+
+     $(this).toggleClass("open");
+     $(".header .row .main-menu").toggleClass("active");
+});
+});
+
+
+ $( function() {
+    $( ".faq .accordion" ).accordion({
+      heightStyle: "content",
+      collapsible: true,
+      active: false,
+    });
+  } );
+
+
+$('.top-banner .container .close').click(function() {
+     $(".top-banner").hide(200);
+});
+
+$('.matches .item .top_block .btn-more').click(function() {
+     $(this).toggleClass("active");
+     $(this).parent().next().toggleClass("hideI");
+});
+
+$('.under-header .container .right_side .premium-btn').change(function() {
+    $(".js-accent").toggleClass("active");
+
+});
+
+
+
+
+
+
+     
